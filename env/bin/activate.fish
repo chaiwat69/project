029a202ba62f29be1611b5e7b86e25f0a357373a
chaiwat69 <chaiwat.sr@srithaigroup.com>
2019-02-26 22:57:25 +0700

    initial commit

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -56,7 +56,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/project/env"
+set -gx VIRTUAL_ENV "/home/ubuntu64/project/env"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
--- env/bin/activate.xsh
+++ env/bin/activate.xsh
@@ -25,7 +25,7 @@ def _deactivate(args):
 _deactivate(["nondestructive"])
 aliases["deactivate"] = _deactivate
 
-$VIRTUAL_ENV = r"/home/ubuntu/project/env"
+$VIRTUAL_ENV = r"/home/ubuntu64/project/env"
 
 $_OLD_VIRTUAL_PATH = $PATH
 $PATH = $PATH[:]
